473a3eb1bb87587e285e6d3543d70c3c384ee35b
Zeeshan Anwar <mdzeeshan4836@gmail.com>
2023-01-13 15:25:55 +0530

    Modified files

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -308,6 +308,7 @@ trackshuffle.addEventListener('click', ()=> {
                 audioElement.playbackRate = fast;                                               // Setting Normal Playback to Fast
                 playspeed.classList.remove('fa-face-smile');
                 playspeed.classList.add('fa-face-dizzy');
+                playspeed.setAttribute("title", "Speed Fast. Click to Change to Slow");
             }
             
             else if(audioElement.playbackRate == fast)
@@ -315,6 +316,7 @@ trackshuffle.addEventListener('click', ()=> {
                 audioElement.playbackRate = slow;                                               // Setting Fast Playback to Slow
                 playspeed.classList.remove('fa-face-dizzy');
                 playspeed.classList.add('fa-face-surprise');
+                playspeed.setAttribute("title", "Speed: Slow. Click to Change to Normal");
             }
             
             else if(audioElement.playbackRate == slow)
@@ -322,6 +324,7 @@ trackshuffle.addEventListener('click', ()=> {
                 audioElement.playbackRate = normal;                                             // Setting Slow Playback to Normal
                 playspeed.classList.remove('fa-face-surprise');
                 playspeed.classList.add('fa-face-smile');
+                playspeed.setAttribute("title", "Speed: Normal. Click to Change to Fast");
             }
             // document.getElementsByClassName('playbackspeed')[0].style.color = "crimson";
         })
@@ -376,6 +379,7 @@ let expanded = 0;
                 // rootelement.classList.add('invert');
                 inverted = 1;
                 invrt.children[0].src = "images/lightbulb-regular.svg"
+                invrt.setAttribute("title", "Toogle to Dark Mode");
 
                 for(p=0; p<=allimgs.length; p++)
                 {
@@ -391,6 +395,7 @@ let expanded = 0;
                 // rootelement.classList.remove('invert');
                 inverted = 0;
                 invrt.children[0].src = "images/lightbulb-solid.svg"
+                invrt.setAttribute("title", "Toogle to Light Mode");
 
                 for(p=0; p<=allimgs.length; p++)
                 {
